7a3c36b6f1e4a3f86dde489b3ab17d56da915717
goldquest <tjwelde@gmail.com>
2016-11-09 12:55:45 +0100

    remove debugging output

--- js/liveblog.stream.js
+++ js/liveblog.stream.js
@@ -1,7 +1,6 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.liveblogStream = {
     attach: function(context, settings) {
-      console.log(settings)
       new LiveblogStream(jQuery('.posts-container', context)[0], {
         getURL: settings.liveblog.getURL,
         getNextURL: settings.liveblog.getNextURL
