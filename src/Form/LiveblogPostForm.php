c2fe72d3e4fa5f4d39806558efa4eaa60283a0fc
Alex Milkovsky <a.milkovsky@gmail.com>
2016-11-11 16:09:36 +0100

    #KUR-41: Added ajax callback for the liveblog add form.

--- src/Form/LiveblogPostForm.php
+++ src/Form/LiveblogPostForm.php
@@ -3,7 +3,7 @@
 namespace Drupal\liveblog\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
-use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -25,12 +25,12 @@ class LiveblogPostForm extends ContentEntityForm {
   /**
    * Constructs a ContentEntityForm object.
    *
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
    *   The entity manager.
    * @param \Symfony\Component\HttpFoundation\RequestStack
    *   The current request.
    */
-  public function __construct(EntityManagerInterface $entity_manager, RequestStack $request_stack) {
+  public function __construct(EntityTypeManagerInterface $entity_manager, RequestStack $request_stack) {
     parent::__construct($entity_manager);
     $this->request = $request_stack->getCurrentRequest();
   }
@@ -59,10 +59,19 @@ class LiveblogPostForm extends ContentEntityForm {
     $form = parent::buildForm($form, $form_state);
 
     if ($node) {
+      $form['#prefix'] = "<div id=\"{$this->getFormId()}-wrapper\">";
+      $form['#suffix'] = '</div>';
+
       // Hide author and liveblog fields, as they are already pre-populated and
       // should not be changed.
       $form['uid']['#access'] = FALSE;
       $form['liveblog']['#access'] = FALSE;
+
+      $form['actions']['submit']['#ajax'] = [
+        'wrapper' => $this->getFormId() . '-wrapper',
+        'callback' => array($this, 'ajaxRebuildCallback'),
+        'effect' => 'fade',
+      ];
     }
 
     if ($entity->isNew()) {
@@ -72,6 +81,32 @@ class LiveblogPostForm extends ContentEntityForm {
       $form['actions']['submit']['#value'] = t('Update');
     }
 
+    return $form;
+  }
+
+  /**
+   * Callback for ajax form submission.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   The rebuilt form.
+   */
+  public function ajaxRebuildCallback(array $form, FormStateInterface $form_state) {
+    drupal_set_message(t('Liveblog post was successfully created'));
+
+    // @todo Clear form values.
+    //$form_state->setRebuild(TRUE);
+    //$form_state->setValues([]);
+    /*$entity = \Drupal::entityTypeManager()->getStorage('liveblog_post')->create([]);
+    $form_object = \Drupal::entityTypeManager()
+      ->getFormObject('liveblog_post', 'add')
+      ->setEntity($entity);*/
+    //$new_form_state = new FormState();
+    //$form = \Drupal::formBuilder()->rebuildForm($this->getFormId(), $form_state);
     return $form;
   }
 
@@ -97,9 +132,9 @@ class LiveblogPostForm extends ContentEntityForm {
   public function save(array $form, FormStateInterface $form_state) {
     $entity = $this->getEntity();
     $entity->save();
-    $url = $entity->toUrl();
 
     if (!$this->getCurrentLiveblogNode()) {
+      $url = $entity->toUrl();
       // Redirect to the post's full page if we are not at the liveblog page.
       $form_state->setRedirect($url->getRouteName(), $url->getRouteParameters());
     }
