e3f2fc12daa70c31f6d3ceabe066531dd462249f
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2016-11-21 18:21:39 +0100

    KUR-47: Fix form response to send ajax commands with the render array.

--- src/Plugin/rest/resource/LiveblogPostFormResource.php
+++ src/Plugin/rest/resource/LiveblogPostFormResource.php
@@ -3,10 +3,13 @@
 namespace Drupal\liveblog\Plugin\rest\resource;
 
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\AjaxResponseAttachmentsProcessor;
+use Drupal\Core\Ajax\PrependCommand;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\rest\Plugin\ResourceBase;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -82,12 +85,30 @@ class LiveblogPostFormResource extends ResourceBase {
         $form_object = $this->entityTypeManager
           ->getFormObject('liveblog_post', 'edit')
           ->setEntity($entity);
-        $form = \Drupal::formBuilder()->getForm($form_object);
-        $variables['form'] = $form;
-        $response = [
-          'form' => render($form),
-        ];
-        return new AjaxResponse($response);
+        $render = \Drupal::formBuilder()->getForm($form_object);
+
+        // Render the form and convert attachments into ajax commands.
+        $html = \Drupal::service('renderer')->renderRoot($render);
+
+        $response = (new AjaxResponse())
+          ->setAttachments($render['#attached']);
+
+        // Take care of adding messages if something generated some.
+        $status_messages = array('#type' => 'status_messages');
+        $output = \Drupal::service('renderer')->renderRoot($status_messages);
+        if (!empty($output)) {
+          $response->addCommand(new PrependCommand(NULL, $output));
+        }
+
+        /** @var AjaxResponseAttachmentsProcessor $processor */
+        $processor = \Drupal::service('ajax_response.attachments_processor');
+        $processor->processAttachments($response);
+
+        $result = array(
+          'commands' => $response->getCommands(),
+          'form' => $html
+        );
+        return new JsonResponse($result);
       }
 
       throw new NotFoundHttpException(t('Entity with ID @id was not found', array('@id' => $id)));
