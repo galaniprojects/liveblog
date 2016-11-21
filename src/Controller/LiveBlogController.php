650c7df92cec119e4ab695ca1b38c2eca434f8bc
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2016-11-21 19:35:05 +0100

    KUR-47: Refactoring the form resource to a regular route.

---
+++ src/Controller/LiveBlogController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\liveblog\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\liveblog\Entity\LiveblogPost;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller providing the resource for the form.
+ */
+class LiveBlogController extends ControllerBase {
+
+  /**
+   * Returns a liveblog post form wrapped in a json response.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   The response containing the liveblog post form.
+   */
+  public function getFormAsJson(LiveblogPost $liveblog_post) {
+    $form_object = $this->entityTypeManager()
+      ->getFormObject('liveblog_post', 'edit')
+      ->setEntity($liveblog_post);
+    $content = $this->formBuilder()->getForm($form_object);
+
+    /** @var \Drupal\liveblog\LiveBlogRendererInterface $renderer */
+    $renderer = \Drupal::service('liveblog.renderer');
+    return new JsonResponse($renderer->render($content));
+  }
+
+}
---
+++ src/LiveBlogRendererInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\liveblog;
+
+/**
+ * Renders content into an array containing ajax commands and html.
+ */
+interface LiveBlogRendererInterface {
+
+  /**
+   * Renders a render array into html and ajax commands.
+   *
+   * @return array
+   *   An array with the following keys and values:
+   *   - html: The rendered html.
+   *   - commands: An array of ajax commands.
+   */
+  public function render(array $content);
+
+}
