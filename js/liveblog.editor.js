0c41fb6c9398ed33798711d13c080806fae6cac3
goldquest <tjwelde@gmail.com>
2016-11-28 14:18:29 +0100

    include edit button

--- js/liveblog.editor.js
+++ js/liveblog.editor.js
@@ -1,22 +1,30 @@
 (function($, Drupal, drupalSettings) {
     Drupal.behaviors.liveblogEditor = {
         attach: function(context, settings) {
+            var self = this
             Drupal.behaviors.liveblogStream.getContainer(context)
                 .once('liveblog-editor-initialised')
                 .each(function(index, element) {
                     var $this = $(element)
-                    $this.on('click','.liveblog-post', function (e) {
+                    $this.on('click','.liveblog-post--edit-button', function (e) {
                         var target = $(e.currentTarget)
-                        var postID = target.data('postid')
-                        if(postID && typeof postID != "undefined") {
-                            var url = settings.liveblog.editFormURL.replace('%d', postID)
-                            // TODO: error handling
-                            $.getJSON(url, function(data) {
-                              target.append(data.form).submit(function(e) {e.preventDefault()})
-                            })
+                        if (!target.hasClass('is-editing')) {
+                            target.addClass('is-editing')
+                            var postID = target.parent().data('postid')
+                            if(postID && typeof postID != "undefined") {
+                                var url = settings.liveblog.editFormURL.replace('%d', postID)
+                                // TODO: error handling
+                                $.getJSON(url, function(data) {
+                              target.append(data.content)
+                                })
+                            }
+                        }
+                        else {
+                            // TODO remove editor
                         }
                     })
-            })
-        }
+                })
+        },
     }
 })(jQuery, Drupal, drupalSettings)
+
